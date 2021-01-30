3e4f3373bc4cf273876cbea6eabb91eaa5a1f6b3
Shahbaaz Rokadia <srokadia@hesabe.com>
2021-01-30 23:25:50 +0300

    Updated(Model): Updated Media model

--- src/app/Models/Media.php
+++ src/app/Models/Media.php
@@ -4,12 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class media extends Model
+class Media extends Model
 {
     use HasFactory;
     protected $fillable = [
         'name',
         'image_path'
     ];
+
+    public function provider(): HasOne
+    {
+        return $this->hasOne(
+            Provider::class,
+            'id',
+            'provider_id'
+        );
+    }
 }
