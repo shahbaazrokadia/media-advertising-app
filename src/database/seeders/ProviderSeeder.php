d6a955b7b6a0210e9171afb06ff0557d60190e26
Shahbaaz Rokadia <srokadia@hesabe.com>
2021-01-30 21:38:19 +0300

    Added(seeders): Added seeders

--- src/database/seeders/ProviderSeeder.php
+++ src/database/seeders/ProviderSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Provider;
-use App\Models\UploadMediaRule;
 use Illuminate\Database\Seeder;
 
 class ProviderSeeder extends Seeder
@@ -15,12 +14,7 @@ class ProviderSeeder extends Seeder
      */
     public function run()
     {
-        UploadMediaRule::create(['provider_id' => '1','media'=>'image','media_type'=>'jpg','aspect_ratio'=>'4:3','max_media_size_mb'=>2]);
-        UploadMediaRule::create(['provider_id' => '1','media'=>'video','media_type'=>'mp4','max_duration_seconds'=>60]);
-        UploadMediaRule::create(['provider_id' => '1','media'=>'audio','media_type'=>'mp3','max_duration_seconds'=>30]);
-        UploadMediaRule::create(['provider_id' => '2','media'=>'image','media_type'=>'jpg','aspect_ratio'=>'16:9','max_media_size_mb'=>5]);
-        UploadMediaRule::create(['provider_id' => '2','media'=>'image','media_type'=>'gif','aspect_ratio'=>'16:9','max_media_size_mb'=>5]);
-        UploadMediaRule::create(['provider_id' => '2','media'=>'video','media_type'=>'mp4','max_duration_seconds'=>300,'max_media_size_mb'=>50]);
-        UploadMediaRule::create(['provider_id' => '2','media'=>'video','media_type'=>'mov','max_duration_seconds'=>300,'max_media_size_mb'=>50]);
+        Provider::create(['name' => 'Google']);
+        Provider::create(['name' => 'Snapchat']);
     }
 }
---
+++ src/database/seeders/UploadMediaRuleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UploadMediaRule;
+use Illuminate\Database\Seeder;
+
+class UploadMediaRuleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        UploadMediaRule::create(['provider_id' => '1','media'=>'image','media_type'=>'jpg','aspect_ratio'=>'4:3','max_media_size_mb'=>2]);
+        UploadMediaRule::create(['provider_id' => '1','media'=>'video','media_type'=>'mp4','max_duration_seconds'=>60]);
+        UploadMediaRule::create(['provider_id' => '1','media'=>'audio','media_type'=>'mp3','max_duration_seconds'=>30]);
+        UploadMediaRule::create(['provider_id' => '2','media'=>'image','media_type'=>'jpg','aspect_ratio'=>'16:9','max_media_size_mb'=>5]);
+        UploadMediaRule::create(['provider_id' => '2','media'=>'image','media_type'=>'gif','aspect_ratio'=>'16:9','max_media_size_mb'=>5]);
+        UploadMediaRule::create(['provider_id' => '2','media'=>'video','media_type'=>'mp4','max_duration_seconds'=>300,'max_media_size_mb'=>50]);
+        UploadMediaRule::create(['provider_id' => '2','media'=>'video','media_type'=>'mov','max_duration_seconds'=>300,'max_media_size_mb'=>50]);
+    }
+}
